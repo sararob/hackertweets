50a2b7ca917cd9f69eadfedd7032b194a095ffcd
sararob <sararob@brandeis.edu>
2013-03-22 14:25:53 -0700

    Fix display bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "HackerTweets"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
+  def h3(page_h3)
+    if page_h3.empty?
+      full_h3 = ""
+    else
+      full_h3 = "Tweets About" + " #{page_h3}"
+    end
+  end
 end
