8177d4ef63ba58efa91106df8adf4a288c5c1830
sararob <sararob@brandeis.edu>
2013-03-22 09:01:48 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
+gem "cocaine", "~> 0.5.0"
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do #use this gem in development and testing only
+  gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackertweets::Application.config.secret_token = '04ee1d3a303745f950f64081c391dd1b471ca259f59c8e0a14498b8c6736cd3a3f6a1591127d7c499991408630b686efacf2150a19ec9b155295e347967365ab'
